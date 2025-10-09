432a331991d48c88cc8a24193f7798697e54dec2
ChaseWright294 <chasew454@gmail.com>
2025-10-08 21:05:39 -0500

    Created baseline content filling for the main menu

---
+++ AR-Plants/Assets/Project Files/Scripts/PopulateMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopulateMenu : MonoBehaviour
+{
+    [SerializeField] private Transform m_Content;
+    [SerializeField] private GameObject m_ButtonPrefab;
+    [SerializeField] private int m_NumPlants; //! Later make this number of plants!
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < m_NumPlants; i++)
+        {
+            GameObject button = Instantiate(m_ButtonPrefab);
+
+            //parent the item to the content panel
+            button.transform.SetParent(m_Content);
+
+            //here will be where the data for each plant will be loaded
+            button.GetComponentInChildren<TMP_Text>().text= "Plant " + (i + 1);
+        }
+    }
+}
