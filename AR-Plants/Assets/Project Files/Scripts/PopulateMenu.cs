04d7313cf4b1c84d8e113699d10a1d608cfe8fc4
ChaseWright294 <chasew454@gmail.com>
2025-10-14 21:53:19 -0500

    Populate Menu now gets the name of folders and assigns that to the button text

--- AR-Plants/Assets/Project Files/Scripts/PopulateMenu.cs
+++ AR-Plants/Assets/Project Files/Scripts/PopulateMenu.cs
@@ -16,8 +16,8 @@ public class PopulateMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        m_NumPlants = Directory.GetFiles(path).Length; //number of plant folders in Prefabs/Plants
+        FileInfo[] plantNames = new DirectoryInfo(path).GetFiles(); //array of files in Prefabs/Plants
+        m_NumPlants = plantNames.Length; //number of plant folders in Prefabs/Plants
         for (int i = 0; i < m_NumPlants; i++)
         {
             GameObject button = Instantiate(m_ButtonPrefab);
@@ -26,7 +26,9 @@ public class PopulateMenu : MonoBehaviour
             button.transform.SetParent(m_Content);
 
             //here will be where the data for each plant will be loaded
-            button.GetComponentInChildren<TMP_Text>().text= "Plant " + (i + 1);
+            string name = plantNames[i].Name;
+            name = name.Substring(0, name.Length - 5); //trim off the file extension of .meta
+            button.GetComponentInChildren<TMP_Text>().text= name;
         }
     }
 }
