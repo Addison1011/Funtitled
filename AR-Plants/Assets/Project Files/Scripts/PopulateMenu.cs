daea528dcd816798a071929192d63c9b2a65192c
AzeezatOA <azee.akin@gmail.com>
2025-10-26 15:59:52 -0500

    Update PopulateMenu.cs

--- AR-Plants/Assets/Project Files/Scripts/PopulateMenu.cs
+++ AR-Plants/Assets/Project Files/Scripts/PopulateMenu.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using SQLite4Unity3d;
+using System.IO;
+using System.Linq;
+using Mono.Cecil.Cil;
 
 public class PopulateMenu : MonoBehaviour
 {
@@ -10,9 +14,37 @@ public class PopulateMenu : MonoBehaviour
     [SerializeField] private GameObject m_ButtonPrefab;
     [SerializeField] private int m_NumPlants; //! Later make this number of plants!
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SQLiteConnection _connection;
+    private List<PlantInfo> plants;
+
+    void Awake()
+    {
+        string dbName = "PlantInfoDB.db";
+        string dbPath = Path.Combine(Application.streamingAssetsPath, dbName);
+
+        Debug.Log("DB Path: " + dbPath);
+
+        _connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+        Debug.Log("Database opened");
+
+        plants = _connection.Table<PlantInfo>().ToList();
+
+        foreach (var plant in plants)
+        {
+            Debug.Log("Plant Part: " + plant.plantName);
+        }
+
+    }
+
+    public class PlantInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int plantID { get; set; }
+        public string plantName { get; set; }
+    }
     void Start()
     {
-        for (int i = 0; i < m_NumPlants; i++)
+        for (int i = 0; i < plants.Count(); i++)
         {
             GameObject button = Instantiate(m_ButtonPrefab);
 
@@ -20,7 +52,7 @@ public class PopulateMenu : MonoBehaviour
             button.transform.SetParent(m_Content);
 
             //here will be where the data for each plant will be loaded
-            button.GetComponentInChildren<TMP_Text>().text= "Plant " + (i + 1);
+            button.GetComponentInChildren<TMP_Text>().text= plants[i].plantName;
         }
     }
 }
