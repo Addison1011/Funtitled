358716dd75ffb70f16d0ef9c33eddd29a18ee35a
chithanhhhh <143448699+chithanhhhh@users.noreply.github.com>
2025-11-05 17:32:11 -0600

    Added pinch to resize the plant

--- AR-Plants/Assets/Resources/Project Files/Scripts/Input/ARInputController.cs
+++ AR-Plants/Assets/Resources/Project Files/Scripts/Input/ARInputController.cs
@@ -152,7 +152,10 @@ public class ARInputController : MonoBehaviour
 
 
 
-
+        if (HitActivePlant(finger.currentTouch.screenPosition))
+        {
+            resizePlantModelOnPinch();
+        }
         // If plant exists and touch is on the plant -> start HOLD candidate
         if (isPlantPlaced && activePlant != null && HitActivePlant(screenPos))
         {
@@ -174,7 +177,7 @@ public class ARInputController : MonoBehaviour
 
     private void OnFingerMove(Finger finger)
     {
-
+        resizePlantModelOnPinch();
         if (!isDragging || activePlant == null || finger != holdFinger) return;
 
         if (TryARRaycastToAllowedPlane(finger.currentTouch.screenPosition, out Pose planePose))
@@ -352,6 +355,35 @@ public class ARInputController : MonoBehaviour
         }
     }
 
+    private void resizePlantModelOnPinch()
+    {
+        if (EnhancedTouch.Touch.activeTouches.Count == 2)
+        {
+            EnhancedTouch.Touch touch0 = EnhancedTouch.Touch.activeTouches[0];
+            EnhancedTouch.Touch touch1 = EnhancedTouch.Touch.activeTouches[1];
+            float maxSize = selectedPlantData.plantInfo.maxSize;
+            float minSize = selectedPlantData.plantInfo.minSize;
+
+            /*if (activePlant.transform.localScale.x < selectedPlantData.plantInfo.minSize &&
+            activePlant.transform.localScale.y < selectedPlantData.plantInfo.minSize &&
+            activePlant.transform.localScale.z < selectedPlantData.plantInfo.minSize)
+            {
+                activePlant.transform.localScale = new Vector3(minSize, minSize, minSize);
+            }
+
+            if (activePlant.transform.localScale.x > selectedPlantData.plantInfo.maxSize &&
+            activePlant.transform.localScale.y > selectedPlantData.plantInfo.maxSize &&
+            activePlant.transform.localScale.z > selectedPlantData.plantInfo.maxSize)
+            {
+                activePlant.transform.localScale = new Vector3(maxSize, maxSize, maxSize);
+            }*/
 
 
+
+            Debug.Log("ScaleX: " + activePlant.transform.localScale.x + " ScaleY: " + activePlant.transform.localScale.y + " ScaleZ: " + activePlant.transform.localScale.z);
+            //Debug.Log("can scale: " + canScale);
+
+
+        }
+    }
 }
