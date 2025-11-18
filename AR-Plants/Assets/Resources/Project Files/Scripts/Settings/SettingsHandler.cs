12b80c315cab5187f25522c4233c024ce9d7547f
ChaseWright294 <chasew454@gmail.com>
2025-11-17 19:29:24 -0600

    Added script to change settings, will need to integrate with upcoming redesigned home page

---
+++ AR-Plants/Assets/Resources/Project Files/Scripts/Settings/SettingsHandler.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SettingsData //data type for the settings
+{
+    public bool soundToggle = true;
+    public int masterVolume = 100;
+    public int musicVolume = 100;
+    public int ambientVolume = 100;
+}
+
+
+public class SettingsHandler : MonoBehaviour
+{
+    public SettingsData settings;
+
+    [SerializeField] private string jsonFilePath = "Project Files/Scripts/Settings/settings";
+    private string persistentPath;
+
+    void Awake()
+    {
+        persistentPath = Path.Combine(Application.persistentDataPath, "settings.json");
+        
+        if(!File.Exists(persistentPath))
+        {
+            TextAsset ta = Resources.Load<TextAsset>(jsonFilePath);
+            if(ta != null)
+            {
+                File.WriteAllText(persistentPath, ta.text);
+            }
+            else
+            {
+                File.WriteAllText(persistentPath, JsonUtility.ToJson(new SettingsData(), true));
+            }
+        }
+
+        LoadSettingsFromJson();
+    }
+
+    //load settings variable with data from json
+    public void LoadSettingsFromJson()
+    {
+        string json = File.ReadAllText(persistentPath);
+        settings = JsonUtility.FromJson<SettingsData>(json);
+    }
+
+    //save settings to json
+    public void SaveSettingsToJson()
+    {
+        string json = JsonUtility.ToJson(settings);
+        File.WriteAllText(persistentPath, json);
+    }
+
+}
