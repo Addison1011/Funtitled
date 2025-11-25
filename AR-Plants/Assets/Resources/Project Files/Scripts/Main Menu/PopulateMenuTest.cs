3214c68173c2999c744402840a2bf62d58b62d6b
Addison Nally <an10-1999@hotmail.com>
2025-11-25 13:39:47 -0600

    added some functionality to view in 3d

--- AR-Plants/Assets/Resources/Project Files/Scripts/Main Menu/PopulateMenuTest.cs
+++ AR-Plants/Assets/Resources/Project Files/Scripts/Main Menu/PopulateMenuTest.cs
@@ -9,6 +9,25 @@ using System.IO;
 using System.Linq;
 
 
+public class PlantInfo
+{
+    [PrimaryKey, AutoIncrement]
+    public int plantID { get; set; }
+    public string plantName { get; set; }
+    public string scientificName { get; set; }
+    public string plantDesc { get; set; }
+    public string stem { get; set; }
+    public string leaf { get; set; }
+    public string flower { get; set; }
+    public string typeID { get; set; }
+
+    public float maxSize = 2;
+    public float minSize = .3f;
+
+    //public string plantModelName = "Nerium oleander";
+    //public string plantModelName = "Monstera";
+}
+
 public class PopulateMenuTest : MonoBehaviour
 {
 
