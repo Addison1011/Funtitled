1d877b6f89ef57370d1a8de7d7753930feb43f58
AzeezatOA <azee.akin@gmail.com>
2026-03-05 00:03:00 -0600

    merged by branch with staging and fixed errors

--- AR-Plants/Assets/Resources/Project Files/Scripts/Main Menu/PopulateMenuTest.cs
+++ AR-Plants/Assets/Resources/Project Files/Scripts/Main Menu/PopulateMenuTest.cs
@@ -147,49 +147,7 @@ public class PopulateMenuTest : MonoBehaviour
 
         if(backButton != null)
         {
-            PlantInfo currentPlant = plants[i];
-
-            // Create a new button from the template
-            VisualElement newPlantCardInstance = plantCardTemplate.CloneTree();
-            Label plantNameLabel = newPlantCardInstance.Q<Label>("PlantName");
-
-            Label scientificNameLabel = newPlantCardInstance.Q<Label>("ScientificName");
-
-            Button button = newPlantCardInstance.Q<Button>("PlantDescriptionButton");
-
-
-            scientificNameLabel.text = currentPlant.scientificName;
-            plantNameLabel.text = currentPlant.plantName;
-
-            //Because Azeezat likes comments: 
-            // Setting the background image of the plant card according to name
-            VisualElement cardElement = newPlantCardInstance.Q<VisualElement>("plant-card");
-            String backgroundImagePath = $"Project Files/Scripts/PlantDescriptionComponent/Images/{currentPlant.plantName}";
-            Texture2D backgroundTexture = Resources.Load<Texture2D>(backgroundImagePath);
-            if (backgroundTexture != null)
-            {
-                cardElement.style.backgroundImage = new StyleBackground(backgroundTexture);
-            }
-            else
-            {
-                //default image is the golden cactus, may want to change this in the future
-                Debug.LogWarning($"Background image not found at path: {backgroundImagePath}");
-
-            }
-
-            // Add the button to the content container
-            content.Add(newPlantCardInstance);
-
-            // Creates a new GameObject holding its own LoadDatabaseInfo script pertaining to the specific plant in the itteration
-            // This allows each button to have its own data loader instance
-            GameObject dataObj = new GameObject($"PlantData_{currentPlant.plantName}");
-            dataObj.transform.SetParent(plantButtonDataHolder.transform, false);
-
-            LoadDatabaseInfo dataLoader = dataObj.AddComponent<LoadDatabaseInfo>();
-            dataLoader.plantInfo = currentPlant;
-
-            //Wire button to its corresponding data loader instance
-            button.clicked += dataLoader.OnClick;
+            backButton.clicked += OnBackButtonClicked;
         }
     }
 
@@ -224,14 +182,29 @@ public class PopulateMenuTest : MonoBehaviour
                 scientificNameLabel.text = currentPlant.scientificName;
                 plantNameLabel.text = currentPlant.plantName;
 
+                //Because Azeezat likes comments: 
+                // Setting the background image of the plant card according to name
+                VisualElement cardElement = newPlantCardInstance.Q<VisualElement>("plant-card");
+                String backgroundImagePath = $"Project Files/Scripts/PlantDescriptionComponent/Images/{currentPlant.plantName}";
+                Texture2D backgroundTexture = Resources.Load<Texture2D>(backgroundImagePath);
+                if (backgroundTexture != null)
+                {
+                    cardElement.style.backgroundImage = new StyleBackground(backgroundTexture);
+                }
+                else
+                {
+                    //default image is the golden cactus, may want to change this in the future
+                    Debug.LogWarning($"Background image not found at path: {backgroundImagePath}");
+
+                }
+
                 // Add the button to the content container
                 content.Add(newPlantCardInstance);
+
                 // Creates a new GameObject holding its own LoadDatabaseInfo script pertaining to the specific plant in the itteration
                 // This allows each button to have its own data loader instance
                 GameObject dataObj = new GameObject($"PlantData_{currentPlant.plantName}");
                 dataObj.transform.SetParent(plantButtonDataHolder.transform, false);
-
-                //Debug.Log(dataObj);
 
                 LoadDatabaseInfo dataLoader = dataObj.AddComponent<LoadDatabaseInfo>();
                 dataLoader.plantInfo = currentPlant;
